aa524a45c669439f8ec903bfb3261a87acdd3b02
warrenbrown <warren@katapultmedia.com>
2018-03-09 21:48:54 -0600

    added styling to index page and forms

--- app.js
+++ app.js
@@ -17,16 +17,9 @@ app.set('view engine', 'mustache');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-
-
-
-
-
-
-
-
+//Set Bootstrap
+app.use('/css/bootstrap.css', express.static('node_modules/bootstrap/dist/css/bootstrap.css'));
+app.use('/css/bootstrap.css', express.static('node_modules/bootstrap/dist/js/bootstrap.js'));
 
 app.listen(3000, function(){
   console.log('Server is running on port 3000')
